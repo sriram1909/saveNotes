b511bc7e61c134c533fc1ee3814a84ab2df828ae
sriram1909 <venkatasriramamballa@gmail.com>
2024-02-01 20:45:25 +0530

    setup frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import { Routes } from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
